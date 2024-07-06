1e575d0989eee1ddbf0fe180d67580fc9b5da470
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-06 11:54:34 +0100

    🎨 ui(app): defined routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,30 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home';
+import Destination from './pages/Destination';
+import Crew from './pages/Crew';
+import Technology from './pages/Technology';
+import PageNotFound from './pages/PageNotFound';
+
+// Components
+import AppLayout from './components/AppLayout';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route index element={<Navigate replace to="home" />} />
+          <Route path="home" element={<Home />} />
+          <Route path="destination" element={<Destination />} />
+          <Route path="crew" element={<Crew />} />
+          <Route path="technology" element={<Technology />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
+  );
 };
+
 export default App;
