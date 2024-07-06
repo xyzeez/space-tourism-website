22cc8b584cb92d700089dd0e4248d07ad002139b
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-06 01:37:41 +0100

    🎨 ui(app): setup system styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      belle: ['Bellefair', 'serif'],
+      barlow: ['Barlow', 'sans-serif'],
+      'barlow-condensed': ['Barlow Condensed', 'sans-serif'],
+    },
+    colors: {
+      white: 'hsl(0,0%,100%)',
+      blue: 'hsl(231,77%,90%)',
+      navy: 'hsl(230,35%,7%)',
+    },
   },
   plugins: [],
 };
