6457a2c823d5330a4754a04e06279a3b07fb2ce4
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-11 23:13:38 +0100

    🎨 ui(app): added left padding for desktop screens

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -3,7 +3,7 @@ import NavMenu from './NavMenu';
 
 const NavBar = () => {
   return (
-    <div className="fixed top-0 xl:top-10 w-full flex items-center justify-between px-6 md:pr-0">
+    <div className="fixed top-0 xl:top-10 w-full flex items-center justify-between px-6 md:pr-0 md:pl-10 xl:pl-16 z-50">
       <Logo />
       <NavMenu />
     </div>
