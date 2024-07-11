f7e273b55b52521e9240e392b63c1ed1c662a7b1
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-11 23:14:50 +0100

    🎨 ui(app): finished styling for responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,25 @@
+import Button from '../components/Button';
+
 const Home = () => {
-  return <main className="bg-home">Home</main>;
+  return (
+    <main className="flex flex-col xl:flex-row items-center md:gap-y-[66px] bg-home p-6 pt-28 md:px-10 md:pt-56 md:pb-32 xl:px-[165px] xl:pt-[360px]">
+      <div className="body text-center xl:text-left text-blue w-full">
+        <h1 className="uppercase mb-6">
+          <span className="heading-xs">So, you want to travel to</span>
+          <span className="block heading-xl text-white mt-6">Space</span>
+        </h1>
+        <p className="max-w-[58ch] xl:max-w-[53ch] mx-auto xl:mx-0">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+      <div className="grid place-content-center xl:place-content-end w-full h-96 md:w-[512px] md:h-fit xl:w-[540px]">
+        <Button>Explore</Button>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
