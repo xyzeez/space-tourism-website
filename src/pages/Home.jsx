77f4494ffc28600a5640b9c951e55c9e19be9551
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-13 00:46:07 +0100

    🎨 ui(app): changed paragraph's color

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
           <span className="heading-xs">So, you want to travel to</span>
           <span className="block heading-xl text-white mt-6">Space</span>
         </h1>
-        <p className="max-w-[58ch] xl:max-w-[53ch] mx-auto xl:mx-0">
+        <p className="text-blue max-w-[58ch] xl:max-w-[53ch] mx-auto xl:mx-0">
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
