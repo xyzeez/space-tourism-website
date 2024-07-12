6f97cff410ec9abb3bb6acbc501049cbd565c3a1
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-13 00:42:38 +0100

    🎨 ui(app): tabs component for destination page

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router-dom';
+
+const Tabs = () => {
+  return (
+    <div className="flex flex-row gap-8 *:pb-[15px] *:nav *:uppercase *:text-blue *:border-b-4 *:border-transparent w-fit">
+      <NavLink
+        to="/destination/moon"
+        className="[&.active]:text-white [&.active]:border-white">
+        Moon
+      </NavLink>
+      <NavLink
+        to="/destination/mars"
+        className="[&.active]:text-white [&.active]:border-white">
+        Mars
+      </NavLink>
+      <NavLink
+        to="/destination/europa"
+        className="[&.active]:text-white [&.active]:border-white">
+        europa
+      </NavLink>
+      <NavLink
+        to="/destination/titan"
+        className="[&.active]:text-white [&.active]:border-white">
+        titan
+      </NavLink>
+    </div>
+  );
+};
+
+export default Tabs;
