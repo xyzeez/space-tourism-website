c0f725cdd72934c19ab3a7d0a6fe86b655dba624
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-13 02:46:33 +0100

    🎨 ui(app): animated active tab link

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,27 +1,50 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Tabs = () => {
+  const { tab } = useParams();
+
   return (
-    <div className="flex flex-row gap-8 *:pb-[15px] *:nav *:uppercase *:text-blue *:border-b-4 *:border-transparent *:transition-colors w-fit">
+    <div className="flex flex-row gap-8 *:pb-[15px] *:nav *:uppercase *:text-blue w-fit">
       <NavLink
         to="/destination/moon"
-        className="[&.active]:text-white [&.active]:border-white">
+        className="relative [&.active]:text-white">
         Moon
+        {tab === 'moon' ? (
+          <motion.span
+            layoutId="tabs"
+            className="absolute -bottom-[1px] left-0 right-0 h-1 bg-white"></motion.span>
+        ) : null}
       </NavLink>
       <NavLink
         to="/destination/mars"
-        className="[&.active]:text-white [&.active]:border-white">
+        className="relative [&.active]:text-white">
         Mars
+        {tab === 'mars' ? (
+          <motion.span
+            layoutId="tabs"
+            className="absolute -bottom-[1px] left-0 right-0 h-1 bg-white"></motion.span>
+        ) : null}
       </NavLink>
       <NavLink
         to="/destination/europa"
-        className="[&.active]:text-white [&.active]:border-white">
+        className="relative [&.active]:text-white">
         europa
+        {tab === 'europa' ? (
+          <motion.span
+            layoutId="tabs"
+            className="absolute -bottom-[1px] left-0 right-0 h-1 bg-white"></motion.span>
+        ) : null}
       </NavLink>
       <NavLink
         to="/destination/titan"
-        className="[&.active]:text-white [&.active]:border-white">
+        className="relative [&.active]:text-white">
         titan
+        {tab === 'titan' ? (
+          <motion.span
+            layoutId="tabs"
+            className="absolute -bottom-[1px] left-0 right-0 h-1 bg-white"></motion.span>
+        ) : null}
       </NavLink>
     </div>
   );
