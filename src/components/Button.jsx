309a5004b9ead10a2a8d9fb38168acb4f355995a
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-13 23:54:54 +0100

    🎨 ui(app): added pagination dots' animation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,9 @@
-const Button = ({
-  children,
-  type = 'display',
-  active = false,
-  clickHandler,
-}) => {
+const Button = ({ children, type = 'display', clickHandler }) => {
   const buttonStyles = {
     display:
       'relative heading-s text-navy text-center uppercase bg-white size-36 md:size-[272px] cursor-pointer z-[1] rounded-full before:absolute before:-top-[88px] before:-bottom-[88px] before:-right-[88px] before:-left-[88px] before:-z-[1] before:bg-transparent before:rounded-full before:transition-colors before:duration-500 before:ease-in-out [&:hover]:before:bg-white/10',
-    'pagination-s': `size-[10px] xl:size-[15px] rounded-full cursor-pointer hover:bg-ashe
-    ${active ? 'bg-white' : 'bg-white/25'}`,
+    'pagination-s':
+      'relative size-[10px] xl:size-[15px] rounded-full cursor-pointer bg-white/25 hover:bg-ashe',
   };
 
   const handleClick = (e) => {
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,24 +1,31 @@
+import { motion } from 'framer-motion';
 import Button from './Button';
 
 const Pagination = ({ activeSlide, slideHandler }) => {
+  const dotStyles = 'absolute top-0 size-full bg-white rounded-full';
+
   return (
     <div className="flex flex-row gap-4 xl:gap-10 w-fit">
-      <Button
-        type="pagination-s"
-        active={activeSlide === 0}
-        clickHandler={() => slideHandler(0)}></Button>
-      <Button
-        type="pagination-s"
-        active={activeSlide === 1}
-        clickHandler={() => slideHandler(1)}></Button>
-      <Button
-        type="pagination-s"
-        active={activeSlide === 2}
-        clickHandler={() => slideHandler(2)}></Button>
-      <Button
-        type="pagination-s"
-        active={activeSlide === 3}
-        clickHandler={() => slideHandler(3)}></Button>
+      <Button type="pagination-s" clickHandler={() => slideHandler(0)}>
+        {activeSlide === 0 ? (
+          <motion.div layoutId="pagination" className={dotStyles} />
+        ) : null}
+      </Button>
+      <Button type="pagination-s" clickHandler={() => slideHandler(1)}>
+        {activeSlide === 1 ? (
+          <motion.div layoutId="pagination" className={dotStyles} />
+        ) : null}
+      </Button>
+      <Button type="pagination-s" clickHandler={() => slideHandler(2)}>
+        {activeSlide === 2 ? (
+          <motion.div layoutId="pagination" className={dotStyles} />
+        ) : null}
+      </Button>
+      <Button type="pagination-s" clickHandler={() => slideHandler(3)}>
+        {activeSlide === 3 ? (
+          <motion.div layoutId="pagination" className={dotStyles} />
+        ) : null}
+      </Button>
     </div>
   );
 };
