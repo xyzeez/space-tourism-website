e945cb5609bdc63ccd0fa154cc02c67e9c2e185d
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-13 01:05:03 +0100

    🎨 ui(app): added transition property

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 const Tabs = () => {
   return (
-    <div className="flex flex-row gap-8 *:pb-[15px] *:nav *:uppercase *:text-blue *:border-b-4 *:border-transparent w-fit">
+    <div className="flex flex-row gap-8 *:pb-[15px] *:nav *:uppercase *:text-blue *:border-b-4 *:border-transparent *:transition-colors w-fit">
       <NavLink
         to="/destination/moon"
         className="[&.active]:text-white [&.active]:border-white">
