fb84e08e27b8fb8a5dd7a1b7f7f9320114e7328a
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-07-13 23:26:52 +0100

    🎨 ui(app): styled pagination component

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,12 +1,24 @@
 import Button from './Button';
 
-const Pagination = () => {
+const Pagination = ({ activeSlide, slideHandler }) => {
   return (
     <div className="flex flex-row gap-4 xl:gap-10 w-fit">
-      <Button type="pagination-s"></Button>
-      <Button type="pagination-s"></Button>
-      <Button type="pagination-s"></Button>
-      <Button type="pagination-s"></Button>
+      <Button
+        type="pagination-s"
+        active={activeSlide === 0}
+        clickHandler={() => slideHandler(0)}></Button>
+      <Button
+        type="pagination-s"
+        active={activeSlide === 1}
+        clickHandler={() => slideHandler(1)}></Button>
+      <Button
+        type="pagination-s"
+        active={activeSlide === 2}
+        clickHandler={() => slideHandler(2)}></Button>
+      <Button
+        type="pagination-s"
+        active={activeSlide === 3}
+        clickHandler={() => slideHandler(3)}></Button>
     </div>
   );
 };
